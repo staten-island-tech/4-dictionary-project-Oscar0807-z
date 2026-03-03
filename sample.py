1b9b7e9bbfb5431aa3211a99fc98dcdc5c26d6b8
Oscar0807-z <oscarz43@nycstudents.net>
2026-03-03 09:24:43 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,13 @@
-x = input("Input sentence")
-y = 0
-z = 0
-for "t" in x:
-    y += 1
-for "s" in x:
-    z += 1
-if y >= z:
-    print ("english")
-if z >= y: 
+
+x = "the red cat sat on the mat"
+t = 0
+s= 0
+for letter in x:
+    if letter.lower == t:
+        t += 1
+    elif letter.lower == s:
+        s += 1
+if t >= s:
+    print ("English")
+if s >= t: 
     print ("French")
