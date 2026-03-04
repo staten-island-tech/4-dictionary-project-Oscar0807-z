c3487b80578b4c0b8fcb2739b2dd027c19695726
Oscar0807-z <oscarz43@nycstudents.net>
2026-03-04 09:00:30 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,13 +1,14 @@
 
-x = "the red cat sat on the mat"
-t = 0
-s= 0
-for letter in x:
-    if letter.lower == t:
-        t += 1
-    elif letter.lower == s:
-        s += 1
-if t >= s:
-    print ("English")
-if s >= t: 
-    print ("French")
+def check_lang(sent):
+    t = 0
+    s= 0
+    for letter in sent:
+        if letter.lower() == "t":
+            t += 1
+        elif letter.lower() == "s":
+            s += 1
+    if t > s:
+        print ("English")
+    elif s >= t: 
+        print ("French")
+check_lang("sssstt")
