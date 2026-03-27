426eee6fd32add36912305ef2fac1611a43d27e6
Oscar0807-z <oscarz43@nycstudents.net>
2026-03-27 08:47:16 -0400

    Create x.py

---
+++ x.py
@@ -0,0 +1,24 @@
+items = [
+ {"name": "Froot Loops", "price": 7.99, "department": "Food", "Description": "Colored rings of cereal for breakfast"},
+ {"name": "Apple", "price": 1.99, "department": "Food", "Description": "A healthy crunchy red fruit"},
+ {"name": "Orange", "price": 2.49, "department": "Food", "Description": "A juicy round orange healthy fruit"},
+ {"name": "Grapes", "price": 3.49, "department": "Food", "Description": "Colorful juicy red or purple fruit"},
+ {"name": "Watermelon", "price": 8.99, "department": "Food", "Description": "A summer seasonal fruit that is juicy when cut into"},
+ {"name": "Sharpie Permanent Markers", "price": 6.99, "department": "School Supplies", "Description": "Permanent marker which is hard to get off"},
+ {"name": "Crayola Colored Pencils", "price": 4.99, "department": "School Supplies", "Description": "Colored pencils for drawing or coloring"},
+ {"name": "Nike Backpack", "price": 94.99, "department": "School Supplies", "Description": "A sports bag which can also be used for school"},
+ {"name": "Muji pens", "price": 14.49, "department": "School Supplies", "Description": "Premium styled pens designed to not smudge"},
+ {"name": "Number two pencils", "price": 7.99, "department": "School Supplies", "Description": "Pencils used for everything you can imagine"},
+ {"name": "Nintendo Switch", "price": 129.99, "department": "Electronics", "Description": "A console for games enabled through game cards or online purchases"},
+ {"name": "AirPods 4th Generation", "price": 168.49, "department": "Electronics", "Description": "Mini headphones used for music or calls"},
+ {"name": "IPhone 17 Pro Max", "price": 879.99, "department": "Electronics", "Description": "A handheld device used for calls or messages"},
+ {"name": "Samsung Desktop PC", "price": 2499.99, "department": "Electronics", "Description": "A desktop computer without a monitor"},
+ {"name": "iPad 8th Generation", "price": 435.99, "department": "Electronics", "Description" : "A handheld device that can be used for school among other things"},
+ {"name": "Kitchen Appliances", "price": 14.49, "department": "Utilities", "Description": "A bundle of knives, spoons, and forks"}, 
+ {"name": "Latex gloves", "price": 8.99, "department": "Utilities", "Description": "Gloves used for cleaning or to keep your hands dry"},
+ {"name": "Bounty paper towels", "price": 17.99, "department": "Utilities", "Description": "Used for soaking up liquid messes"},
+ {"name": "Scott Toilet paper", "price": 14.99, "department": "Utilities", "Description": "Pack of toilet paper used for cleanliness"},
+ {"name": "Kleenex tissue paper", "price": 18.99, "department": "Utilities", "Description": "A 12 pack of tissues to be discarded after use"}
+]
+for item in items:
+     print(item["name"])
